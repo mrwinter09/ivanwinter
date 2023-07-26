bea85887b14b943037545eccc0442a2e7d26f4a2
mrwinter09 <ivan.winter09@gmail.com>
2023-07-26 21:41:04 +0200

    chek

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -6,6 +6,7 @@ import Link from 'next/link'
 import { AiOutlineMail } from 'react-icons/ai'
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
+import { HiOutlineChevronDoubleUp } from 'react-icons/hi'
 import contactImg from '../../public/assets/contact.jpg'
 
 const Contact = () => {
@@ -68,6 +69,61 @@ const Contact = () => {
           </div>
 
           {/* Right */}
+          <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
+            <div className="p-4">
+              <form>
+                <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+                  <div className="flex flex-col">
+                    <label className="uppercase text-sm py-2">Name</label>
+                    <input
+                      className="border-2 rounded-lg p-3 flex border-gray-300"
+                      type="text"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label className="uppercase text-sm py-2">
+                      Phone Number
+                    </label>
+                    <input
+                      className="border-2 rounded-lg p-3 flex border-gray-300"
+                      type="number"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="uppercase text-sm py-2">Email</label>
+                  <input
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    type="email"
+                  />
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="uppercase text-sm py-2">Subject</label>
+                  <input
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    type="text"
+                  />
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className="uppercase text-sm py-2">Subject</label>
+                  <textarea
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    rows="10"
+                  />
+                </div>
+                <button className="w-full p-4 text-gray mt-4">
+                  Send message
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center py-12">
+          <Link href="/">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
+              <HiOutlineChevronDoubleUp size={30} className="text-[#5651e5]" />
+            </div>
+          </Link>
         </div>
       </div>
     </div>
--- components/Projects/ProjectItem.js
+++ components/Projects/ProjectItem.js
@@ -6,7 +6,7 @@ import Link from 'next/link'
 
 const ProjectItem = ({ image, title, projectUrl }) => {
   return (
-    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xp p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
+    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
       <Image
         className="rounded-xl group-hover:opacity-10"
         src={image}
